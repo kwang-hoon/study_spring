591debbb86728ee8fedd3dcbe152b5ec692ce4f8
KITRI <kwanghoon222@gmailc.om>
2020-02-04 17:12:50 +0900

    datasource setting

--- src/main/java/kr/re/kitri/kitribook/dao/PostDao.java
+++ src/main/java/kr/re/kitri/kitribook/dao/PostDao.java
@@ -30,9 +30,9 @@ public class PostDao {
     public List<Post> selectAllPosts() {
         // select * from post
         List<Post> books = new ArrayList<>();
-        Post book1 = new Post("kim", "no1", "aaaa", 10);
-        Post book2 = new Post("lee", "no3", "bbbb", 10);
-        Post book3 = new Post("park", "no2", "cccc", 10);
+        Post book1 = new Post(2, "kim", "no1", "aaaa", 10);
+        Post book2 = new Post(3, "lee", "no3", "bbbb", 10);
+        Post book3 = new Post(4, "park", "no2", "cccc", 10);
         books.add(book1);
         books.add(book2);
         books.add(book3);
@@ -41,7 +41,7 @@ public class PostDao {
 
     public Post selectPostByKey(long bookId) {
         // select * from book where book_id/bookId
-        Post post = new Post("park", "too many money", "hi", 0);
+        Post post = new Post(5, "park", "too many money", "hi", 0);
 
         return post;
     }
--- src/main/java/kr/re/kitri/kitribook/model/Post.java
+++ src/main/java/kr/re/kitri/kitribook/model/Post.java
@@ -4,6 +4,7 @@ package kr.re.kitri.kitribook.model;
 // author, title, content, likes
 
 public class Post {
+    private long postId;
     private String author;
     private String title;
     private String content;
@@ -11,12 +12,19 @@ public class Post {
 
     public Post() {}
 
-    public Post(String author, String title, String content, long likes) {
+
+
+    public Post(long postId, String author, String title, String content, long likes) {
+        this.postId = postId;
         this.author = author;
         this.title = title;
         this.content = content;
         this.likes = likes;
     }
+
+    public long getPostId() { return postId; }
+
+    public void setPostId(long postId) { this.postId = postId; }
 
     public String getAuthor() {
         return author;
@@ -53,6 +61,7 @@ public class Post {
     @Override
     public String toString() {
         return "Post{" +
+                "postId'" + postId + '\'' +
                 "author='" + author + '\'' +
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
